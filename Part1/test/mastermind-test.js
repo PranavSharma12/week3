459518b8eb5b33a74f82dc3f74c1dde821f00587
PranavSharma12 <pranav.cu2017@gmail.com>
2022-05-26 04:11:24 +0530

    Improved

--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,65 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const chai = require("chai");
+const path = require("path");
+
+const wasm_tester = require("circom_tester").wasm;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+const assert = chai.assert;
+
+describe("MasterMindVariation Test", function () {
+    this.timeout(100000000);
+
+    // it("Bonus question", async () => {
+    //     const circuit = await wasm_tester("contracts/bonus/SystemOfEquations.circom");
+    //     await circuit.loadConstraints();
+
+    //     const INPUT = {
+    //         "x": ["15","17","19"],
+    //         "A": [["1","1","1"],["1","2","3"],["2",Fr.e(-1),"1"]],
+    //         "b": ["51", "106", "32"]
+    //     }
+
+    //     const witness = await circuit.calculateWitness(INPUT, true);
+
+    //     console.log(witness);
+
+    //     assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
+    //     assert(Fr.eq(Fr.e(witness[1]),Fr.e(1)));
+    it("Should be true Witness", async () => {
+        const circuit = await wasm_tester(
+          "contracts/circuits/MastermindVariation.circom"
+        );
+        await circuit.loadConstraints();
+    
+        const INPUT = {
+            pubGuessA: "1",
+            pubGuessB: "2",
+            pubGuessC: "3",
+            pubGuessD: "4",
+            pubGuessE: "5",
+      
+            pubNumHit: "5",
+            pubNumBlow: "0",
+            pubSolnHash:
+              "28068473783840770531767888194305787151601999113342389726424607172616490045923",
+      
+            privSolnA: "1",
+            privSolnB: "2",
+            privSolnC: "3",
+            privSolnD: "4",
+            privSolnE: "5",
+            privSalt: "1234",
+          };
+        const witness = await circuit.calculateWitness(INPUT);
+  //      console.log("Witness:",witness);
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert.equal(witness[0], 1n);
+
+    });
+});
